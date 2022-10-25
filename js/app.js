884aded5972a196a445c6ee28780655a6dff5323
Pedro Morante <pedro.morante@outlook.com>
2022-10-25 10:51:23 -0500

    add validation form

--- js/app.js
+++ js/app.js
@@ -5,5 +5,5 @@ const inputs = document.querySelectorAll('input');
 inputs.forEach((input) => {
   input.addEventListener('blur', (input) => {
     valida(input.target);
-  })
+  });  
 })
--- js/validacion.js
+++ js/validacion.js
@@ -3,7 +3,50 @@ export const valida = input => {
   if (validadores[tipoInput]) {
     validadores[tipoInput](input);
   }
+  if (input.validity.valid) {
+    input.parentElement.classList.remove('input-container--invalid');
+    input.parentElement.querySelector('.input-message-error').innerHTML = '';
+  } else {
+    input.parentElement.classList.add('input-container--invalid');
+    input.parentElement.querySelector('.input-message-error').innerHTML = mostrarMensajeError(tipoInput, input);
+  }
 }
+
+const tipoError = [
+  'valueMissing',
+  'typeMismatch',
+  'patternMismatch',
+  'customError',
+];
+
+const mensajeError = {
+  nombre: {
+    valueMissing: 'Este campo nombre no puede estar vacio',
+  },
+  email: {
+    valueMissing: 'Este campo no puede estar vacio',
+    typeMismatch: 'El correo no es válido',
+  },
+  password: {
+    valueMissing: 'Este campo no puede estar vacio',
+    patternMismatch:
+      'Mínimo 6 caracteres, máximo 12 caracteres. Debe contener una letra mayuscula, un número y no deben existir caracteres especiales',
+  },
+  nacimiento: {
+    valueMissing: 'Este campo no puede estar vacio',
+    customError: 'Debes tener al menos 18 años',
+  },
+};
+
+const mostrarMensajeError = (tipoInput, input) => {
+  let mensaje = '';
+  tipoError.forEach(error => {
+    if (input.validity[error]) {
+      mensaje = mensajeError[tipoInput][error];
+    }
+  })
+  return mensaje;
+};
 
 const validadores = {
   nacimiento: (input) => validarNacimiento(input),
